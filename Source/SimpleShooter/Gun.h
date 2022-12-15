c5a23297afd00ee9a4fbab16f8b7eac4677bc4c5
MaxiMod <114616369+Modie-S@users.noreply.github.com>
2022-12-15 01:57:16 +0000

    Add files via upload

---
+++ Source/SimpleShooter/Gun.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AGun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+
+	void PullTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* MuzzleFlash;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* MuzzleSound;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactEffect;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* ImpactSound;
+
+	UPROPERTY(EditAnywhere)
+	float MaxRange = 1000;
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 10;
+
+	UPROPERTY(EditDefaultsOnly)
+	int MaxAmmo = 100;
+
+	UPROPERTY(VisibleAnywhere)
+	int Ammo;
+
+	UFUNCTION(BlueprintPure)
+	int GetAmmo();
+
+	UFUNCTION(BlueprintPure)
+	bool OutOfAmmo() const;
+
+	
+	
+	
+	bool GunTrace(FHitResult& Hit, FVector& ShotDirection);
+
+	AController* GetOwnerController() const;
+
+
+
+};
